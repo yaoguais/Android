4c25e2bb6948a862ac30050e38f61ce7893fdbc9
Yaoguais <newtopstdio@163.com>
2016-08-16 09:25:52 +0800

    https-api

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Android On the Way
 
 - [基于jegarn的聊天系统](./android-chat-system)
+- [基于okhttp的ssl双向认证](./https-api)
 
---
+++ https-api/app/src/main/java/com/jegarn/https_api/MainActivity.java
@@ -0,0 +1,92 @@
+package com.jegarn.https_api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+import com.zhy.http.okhttp.https.HttpsUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.util.Enumeration;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.sslRequest();
+    }
+
+    protected void sslRequest() {
+
+        // wiki: http://blog.csdn.net/lmj623565791/article/details/48129405
+
+        InputStream certificates = getResources().openRawResource(R.raw.server);
+        InputStream pkcs12File = getResources().openRawResource(R.raw.client);
+        String password = "export111111";
+        InputStream bksFile = this.pkcs12ToBks(pkcs12File, password);
+        HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(new InputStream[]{certificates}, bksFile, password);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager)
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
+
+        String url = "https://yaoguai.com:7080/index.php";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        System.out.println(response);
+                    }
+                });
+    }
+
+    protected InputStream pkcs12ToBks(InputStream pkcs12Stream, String pkcs12Password) {
+        final String alias = "0943d12a6386473001b91b17fa2263d8235a869b";
+        final char[] password = pkcs12Password.toCharArray();
+        try {
+            KeyStore pkcs12 = KeyStore.getInstance("PKCS12");
+            pkcs12.load(pkcs12Stream, password);
+            Certificate certificate = pkcs12.getCertificate(alias);
+            Enumeration<String> aliases = pkcs12.aliases();
+            while(aliases.hasMoreElements()){
+                System.out.println("alias: " + aliases.nextElement());
+            }
+            final Key key = pkcs12.getKey(alias, password);
+            KeyStore bks = KeyStore.getInstance("BKS");
+            bks.load(null, password);
+            bks.setKeyEntry(alias, key, password, new Certificate[]{certificate});
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            bks.store(out, password);
+            return new ByteArrayInputStream(out.toByteArray());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
